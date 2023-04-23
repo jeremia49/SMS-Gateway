b1e98759c8d29f92707cf7420e719decb55faaa1
jeremia49 <39850621+jeremia49@users.noreply.github.com>
2023-04-23 08:23:36 +0700

    update api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\sms;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,51 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/sms', function (Request $request) {
-    return $request;
+    if(null !== $request->input('unprocessed')){
+        $smss = sms::where('status', 0)->orderBy('created_at', 'ASC')->get();
+    }else{
+        $smss = sms::orderBy('created_at', 'ASC')->get();
+    }
+    $ids = array();
+    foreach ($smss as $sm) {
+        array_push($ids,$sm->id);
+    }
+    return $ids;
 });
+
+
+Route::get('/getSMS/{id}', function (int $id) {
+    $sm = sms::find($id);
+    return $sm;
+});
+
+Route::get('/procSMS/{id}', function (int $id) {
+    $sm = sms::find($id);
+    $sm->status = 1;
+    $sm->save();
+    return $sm;
+});
+
+Route::get('/setSuccess/{id}', function (int $id) {
+    $sm = sms::find($id);
+    $sm->status = 2;
+    $sm->save();
+    return $sm;
+});
+
+Route::get('/setFailed/{id}', function (int $id) {
+    $sm = sms::find($id);
+    $sm->status = -1;
+    $sm->save();
+    return $sm;
+});
+
+Route::post('/createSMS', function (Request $request) { 
+    $sms = new sms;
+    $sms->number = $request->input('number');
+    $sms->content = $request->input('content');
+    $sms->save();
+    return $sms->id;
+});
+
+
